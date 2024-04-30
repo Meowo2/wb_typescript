a4769fc1ecdb3e091fca9f8d8fee51358f296400
Arthur Sousa <112907764+Meowo2@users.noreply.github.com>
2024-04-30 09:10:20 -0300

    [feat]: Added product inventory feature

--- src/negocio/cadastroProduto.ts
+++ src/negocio/cadastroProduto.ts
@@ -17,8 +17,9 @@ export default class CadastroProduto extends Cadastro {
         let nome = this.entrada.receberTexto(`Por favor informe o nome do produto: `);
         let preco = this.entrada.receberNumero(`Por favor informe o preço do produto: `);
         let codigo = this.entrada.receberTexto(`Por favor informe o código do produto: `);
+        let quantidade = this.entrada.receberNumero(`Por favor informe a quantidade do produto: `);
 
-        let produto = new Produto(nome, preco, codigo);
+        let produto = new Produto(nome, preco, codigo, quantidade);
         this.produtos.push(produto);
 
         console.log(`\nCadastro concluído :)\n`);
@@ -26,31 +27,31 @@ export default class CadastroProduto extends Cadastro {
 
     public preCadastrados(): void {
         let produtosIniciais = [
-            { nome: 'Base Líquida', preco: 39.99, codigo: '123' },
-            { nome: 'Máscara de Cílios', preco: 19.99, codigo: '456' },
-            { nome: 'Batom Vermelho', preco: 14.99, codigo: '789' },
-            { nome: 'Pó Compacto', preco: 29.99, codigo: '012' },
-            { nome: 'Blush Rosa', preco: 9.99, codigo: '345' },
-            { nome: 'Delineador Preto', preco: 12.99, codigo: '678' },
-            { nome: 'Corretivo Líquido', preco: 19.99, codigo: '901' },
-            { nome: 'Sombra Marrom', preco: 9.99, codigo: '234' },
-            { nome: 'Batom Nude', preco: 14.99, codigo: '567' },
-            { nome: 'Iluminador em Pó', preco: 24.99, codigo: '890' },
+            { nome: 'Base Líquida', preco: 39.99, codigo: '123', quantidade: 8 },
+            { nome: 'Máscara de Cílios', preco: 19.99, codigo: '456', quantidade: 10 },
+            { nome: 'Batom Vermelho', preco: 14.99, codigo: '789', quantidade: 7 },
+            { nome: 'Pó Compacto', preco: 29.99, codigo: '012', quantidade: 10 },
+            { nome: 'Blush Rosa', preco: 9.99, codigo: '345', quantidade: 5 },
+            { nome: 'Delineador Preto', preco: 12.99, codigo: '678', quantidade: 10 },
+            { nome: 'Corretivo Líquido', preco: 19.99, codigo: '901', quantidade: 4 },
+            { nome: 'Sombra Marrom', preco: 9.99, codigo: '234', quantidade: 10 },
+            { nome: 'Batom Nude', preco: 14.99, codigo: '567', quantidade: 2 },
+            { nome: 'Iluminador em Pó', preco: 24.99, codigo: '890', quantidade: 1 },
 
-            { nome: 'Perfume Masculino', preco: 99.99, codigo: '123' },
-            { nome: 'Gel para Cabelo', preco: 14.99, codigo: '456' },
-            { nome: 'Desodorante Roll-on', preco: 9.99, codigo: '789' },
-            { nome: 'Creme para Barbear', preco: 19.99, codigo: '012' },
-            { nome: 'Loção Pós-Barba', preco: 12.99, codigo: '345' },
-            { nome: 'Shampoo 2 em 1', preco: 15.99, codigo: '678' },
-            { nome: 'Gel Fixador para Sobrancelhas', preco: 11.99, codigo: '901' },
-            { nome: 'Gel de Limpeza Facial', preco: 17.99, codigo: '234' },
-            { nome: 'Sabonete Líquido Corporal', preco: 8.99, codigo: '567' },
-            { nome: 'Creme Hidratante Facial', preco: 29.99, codigo: '890' }
+            { nome: 'Perfume Masculino', preco: 99.99, codigo: '123', quantidade: 6 },
+            { nome: 'Gel para Cabelo', preco: 14.99, codigo: '456', quantidade: 9 },
+            { nome: 'Desodorante Roll-on', preco: 9.99, codigo: '789', quantidade: 3 },
+            { nome: 'Creme para Barbear', preco: 19.99, codigo: '012', quantidade: 10 },
+            { nome: 'Loção Pós-Barba', preco: 12.99, codigo: '345', quantidade: 2 },
+            { nome: 'Shampoo 2 em 1', preco: 15.99, codigo: '678', quantidade: 7 },
+            { nome: 'Gel Fixador para Sobrancelhas', preco: 11.99, codigo: '901', quantidade: 10 },
+            { nome: 'Gel de Limpeza Facial', preco: 17.99, codigo: '234', quantidade: 8 },
+            { nome: 'Sabonete Líquido Corporal', preco: 8.99, codigo: '567', quantidade: 9 },
+            { nome: 'Creme Hidratante Facial', preco: 29.99, codigo: '890', quantidade: 1}
         ];
 
         for (let produtoInicial of produtosIniciais) {
-            let produto = new Produto(produtoInicial.nome, produtoInicial.preco, produtoInicial.codigo);
+            let produto = new Produto(produtoInicial.nome, produtoInicial.preco, produtoInicial.codigo, produtoInicial.quantidade);
             this.produtos.push(produto);
         }
     }
