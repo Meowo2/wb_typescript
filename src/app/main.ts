6e44c02d465a9f9cf9ecb0f4cda0d42c84a24d68
Arthur Sousa <112907764+Meowo2@users.noreply.github.com>
2024-05-03 09:36:19 -0300

    [feat]: Adds deletion options for customer, product, and service

--- src/app/main.ts
+++ src/app/main.ts
@@ -11,6 +11,9 @@ import ListagemVendas from "../negocio/listagemVendas";
 import EditarCliente from "../negocio/editarCliente";
 import EditarProduto from "../negocio/editarProduto";
 import EditarServico from "../negocio/editarServico";
+import ExcluirCliente from "../negocio/excluirCliente";
+import ExcluirProduto from "../negocio/excluirProduto";
+import ExcluirServico from "../negocio/excluirServico";
 
 
 
@@ -39,17 +42,20 @@ while (execucao) {
     console.log(`1 - Cadastrar cliente`);
     console.log(`2 - Listar todos os clientes`);
     console.log(`3 - Editar cliente`)
+    console.log(`4 - Excluir cliente`)
 
-    console.log(`4 - Cadastrar produto`);
-    console.log(`5 - Listar todos os produtos`);
-    console.log(`6 - Editar produto`);
+    console.log(`5 - Cadastrar produto`);
+    console.log(`6 - Listar todos os produtos`);
+    console.log(`7 - Editar produto`);
+    console.log(`8 - Excluir cliente`)
 
-    console.log(`7 - Cadastrar serviço`);
-    console.log(`8 - Listar todos os serviços`);
-    console.log(`9 - Editar serviço`);
+    console.log(`9 - Cadastrar serviço`);
+    console.log(`10 - Listar todos os serviços`);
+    console.log(`11 - Editar serviço`);
+    console.log(`12 - Excluir cliente`)
 
-    console.log(`10 - Cadastrar venda`);
-    console.log(`11 - Listar todas as vendas`);
+    console.log(`13 - Cadastrar venda`);
+    console.log(`14 - Listar todas as vendas`);
     console.log(`0 - Sair`);
 
     let entrada = new Entrada()
@@ -71,39 +77,54 @@ while (execucao) {
             empresa.atualizarClientes(editarCliente.getClientes)
             break;
         case 4:
+            let excluirCliente = new ExcluirCliente(empresa.getClientes)
+            excluirCliente.excluir()
+            empresa.atualizarClientes(excluirCliente.getClientes)
+            break;
+        case 5:
             //let cadastroProduto = new CadastroProduto(empresa.getProdutos)
             cadastroProduto.cadastrar()
             empresa.atualizarProdutos(cadastroProduto.getProdutos)
             break;
-        case 5:
+        case 6:
             let listagemProdutos = new ListagemProdutos(empresa.getProdutos)
             listagemProdutos.listar()
             break;
-        case 6:
+        case 7:
             let editarProduto = new EditarProduto(empresa.getProdutos)
             editarProduto.editar()
             empresa.atualizarProdutos(editarProduto.getProdutos)
             break;
-        case 7:
+        case 8:
+            let excluirProduto = new ExcluirProduto(empresa.getProdutos)
+            excluirProduto.excluir()
+            empresa.atualizarProdutos(excluirProduto.getProdutos)
+            break;
+        case 9:
             //let cadastroServico = new CadastroServico(empresa.getServicos)
             cadastroServico.cadastrar()
             empresa.atualizarServicos(cadastroServico.getServicos)
             break;
-        case 8:
+        case 10:
             let listagemServicos = new ListagemServicos(empresa.getServicos)
             listagemServicos.listar()
             break;
-        case 9:
+        case 11:
             let editarServico = new EditarServico(empresa.getServicos)
             editarServico.editar()
             empresa.atualizarServicos(editarServico.getServicos)
             break;
-        case 10:
+        case 12:
+            let excluirServico = new ExcluirServico(empresa.getServicos)
+            excluirServico.excluir()
+            empresa.atualizarServicos(excluirServico.getServicos)
+            break;
+        case 13:
             //let cadastroVenda = new CadastroVenda(empresa.getVendas, empresa.getClientes, empresa.getProdutos, empresa.getServicos)
             cadastroVenda.cadastrar()
             empresa.atualizarVendas(cadastroVenda.getVendas)
             break;
-        case 11:
+        case 14:
             let listagemVendas = new ListagemVendas(empresa.getVendas)
             listagemVendas.listar() 
             break;
