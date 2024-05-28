d629b7d773383316a4217641494ec4cff1970b46
Arthur Sousa <112907764+Meowo2@users.noreply.github.com>
2024-05-28 08:34:52 -0300

    [feat]: Updated customer registration fields and added option to select gender in registration form

--- src/componentes/formularioCadastroCliente.tsx
+++ src/componentes/formularioCadastroCliente.tsx
@@ -14,22 +14,34 @@ export default class FormularioCadastroCliente extends Component<props> {
                     <div className="row">
                         <div className="input-field col s6">
                             <input id="first_name" type="text" className="validate" />
-                            <label htmlFor="first_name">nome</label>
+                            <label htmlFor="first_name">Nome Completo</label>
                         </div>
                         <div className="input-field col s6">
                             <input id="last_name" type="text" className="validate" />
-                            <label htmlFor="last_name">sobrenome</label>
+                            <label htmlFor="last_name">Nome Social</label>
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s6">
                             <input id="telefone" type="text" className="validate" />
-                            <label htmlFor="telefone">telefone</label>
+                            <label htmlFor="telefone">CPF</label>
                         </div>
                         <div className="input-field col s6">
                             <input id="email" type="email" className="validate" />
-                            <label htmlFor="email">e-mail</label>
+                            <label htmlFor="email">Telefone</label>
                         </div>
+                    </div>
+                    <div className="row">
+                        <p>
+                            <label>
+                                <input name="group1" type="radio" checked />
+                                <span>Masculino</span>
+                            </label>
+                            <label>
+                                <input name="group1" type="radio" />
+                                <span>Feminino</span>
+                            </label>
+                        </p>
                     </div>
                     <div className="row">
                         <div className="col s12">
