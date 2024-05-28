452cfb34ac7c5ffa300502a31c37052e364265cd
Arthur Sousa <112907764+Meowo2@users.noreply.github.com>
2024-05-28 08:44:22 -0300

    [feat]: Added form classes for product and service registration

---
+++ src/componentes/formularioCadastroProduto.tsx
@@ -0,0 +1,44 @@
+import { Component } from "react";
+
+type Props = {
+    theme: string;
+};
+
+export default class FormularioCadastroProduto extends Component<Props> {
+    render() {
+        const buttonStyle = `btn waves-effect waves-light ${this.props.theme}`;
+        return (
+            <div className="row">
+                <form className="col s12">
+                    <div className="row">
+                        <div className="input-field col s6">
+                            <input id="product_name" type="text" className="validate" />
+                            <label htmlFor="product_name">Nome</label>
+                        </div>
+                        <div className="input-field col s6">
+                            <input id="price" type="number" className="validate" />
+                            <label htmlFor="price">Preço</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <textarea id="product_code" className="materialize-textarea"></textarea>
+                            <label htmlFor="product_code">Código</label>
+                        </div>
+                        <div className="input-field col s12">
+                            <textarea id="product_amount" className="materialize-textarea"></textarea>
+                            <label htmlFor="product_amount">Quantidade</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col s12">
+                            <button className={buttonStyle} type="submit" name="action">Submit
+                                <i className="material-icons right">send</i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
---
+++ src/componentes/formularioCadastroServico.tsx
@@ -0,0 +1,41 @@
+import { Component } from "react";
+
+type Props = {
+    theme: string;
+};
+
+export default class FormularioCadastroServico extends Component<Props> {
+    render() {
+        const buttonStyle = `btn waves-effect waves-light ${this.props.theme}`;
+        return (
+            <div className="row">
+                <form className="col s12">
+                    <div className="row">
+                        <div className="input-field col s6">
+                            <input id="service_name" type="text" className="validate" />
+                            <label htmlFor="service_name">Nome</label>
+                        </div>
+                        <div className="input-field col s6">
+                            <input id="service_price" type="text" className="validate" />
+                            <label htmlFor="service_price">Preço</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s6">
+                            <input id="service_code" type="text" className="validate" />
+                            <label htmlFor="service_code">Código</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col s12">
+                            <button className={buttonStyle} type="submit" name="action">
+                                Submit
+                                <i className="material-icons right">send</i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
