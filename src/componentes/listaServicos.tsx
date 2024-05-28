650e57ba434c65532088ce9d51e033d8bb6f03f5
Arthur Sousa <112907764+Meowo2@users.noreply.github.com>
2024-05-28 08:51:25 -0300

    [feat]: Added button to access product and service registration page

--- src/componentes/listaServicos.tsx
+++ src/componentes/listaServicos.tsx
@@ -1,22 +1,52 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { Component } from "react";
 import 'materialize-css/dist/css/materialize.min.css'
+import FormularioCadastroServico from "./formularioCadastroServico";
 
 
-type props = {
+type Props = {
     tema: string
 }
 
-export default class ListaServicos extends Component<props> {
+export default class ListaServicos extends Component<Props, { mostrarCadastro: boolean }> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            mostrarCadastro: false
+        };
+    }
+
+    handleAddClick = () => {
+        this.setState({ mostrarCadastro: true });
+    }
+
     render() {
-        let classeAtiva = `collection-item active ${this.props.tema}`
-        return (
-            <div className="collection">
-                <a className="collection-item">Serviço 1</a>
-                <a className={classeAtiva}>Serviço 2</a>
-                <a className="collection-item">Serviço 3</a>
-                <a className="collection-item">Serviço 4</a>
-            </div>
-        )
+        let estilo = `collection-item active ${this.props.tema}`;
+        if (this.state.mostrarCadastro) {
+            return (
+                <>
+                    <div>
+                        <FormularioCadastroServico tema="purple lighten-4" />
+                    </div>
+                    <div className="right-align">
+                        <a onClick={() => this.setState({ mostrarCadastro: false })} className="waves-effect waves-light btn">Voltar</a>
+                    </div>
+                </>
+            );
+        } else {
+            return (
+                <>
+                    <div className="collection">
+                        <a className="collection-item">Serviço 1</a>
+                        <a className={estilo}>Serviço 2</a>
+                        <a className="collection-item">Serviço 3</a>
+                        <a className="collection-item">Serviço 4</a>
+                    </div>
+                    <div className="right-align">
+                        <a className="btn-floating btn-large waves-effect waves-light red" onClick={this.handleAddClick}><i className="material-icons">add</i></a>
+                    </div>
+                </>
+            );
+        }
     }
 }
